5a7ee96a0c5189c273a0d3c8815aa128f31341f3
Christos Marroquin <christos.marroquin@hotmail.com>
2022-11-11 01:18:14 -0600

    usuario form

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -5,12 +5,13 @@ from django.contrib.auth.decorators import login_required
 # Funciones para login y logout
 from django.contrib.auth.views import logout_then_login, LoginView
 from apps.libro.views import Inicio
+from apps.usuario.views import Login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("inicio", login_required(Inicio.as_view()), name="index"),
     path("libro/", include(("apps.libro.urls", "libro"))),
     # LoginView con el parametro template_name para dirigir el login a ese template.
-    path("", LoginView.as_view(template_name='login.html', redirect_authenticated_user=True) , name="login"),
+    path("", Login.as_view() , name="login"),
     path("logout/", logout_then_login, name="logout")
 ]
