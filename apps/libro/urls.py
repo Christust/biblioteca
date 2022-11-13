af66fe38496148b1438fe61b3d9650524cf99e56
Christos Marroquin <christos.marroquin@hotmail.com>
2022-11-12 23:06:40 -0600

    cambios autores e inicio libros

--- apps/libro/urls.py
+++ apps/libro/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("crear_autor/", login_required(views.CrearAutor.as_view()), name="crear_autor"),
     path("listar_autor", login_required(views.ListadoAutor.as_view()), name="listar_autor"),
     path("editar_autor/<int:pk>", login_required(views.ActualizarAutor.as_view()), name="editar_autor"),
-    path("eliminar_autor/<int:pk>", login_required(views.EliminarAutor.as_view()), name="eliminar_autor")
+    path("eliminar_autor/<int:pk>", login_required(views.EliminarAutor.as_view()), name="eliminar_autor"),
+    path("listar_libros/", views.ListarLibro.as_view(), name="listar_libro")
 ]
--- apps/libro/views.py
+++ apps/libro/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
-from django.core.exceptions import ObjectDoesNotExist
-from .models import Autor
-from . import forms
 from django.views.generic import TemplateView, View, ListView, UpdateView, CreateView, DeleteView
 from django.urls import reverse_lazy
+from django.core.exceptions import ObjectDoesNotExist
+
+from .models import Autor, Libro
+from . import forms
 # Create your views here.
 
 class Inicio(TemplateView):
@@ -11,24 +12,25 @@ class Inicio(TemplateView):
 
 class ListadoAutor(ListView):
     model = Autor
-    template_name: str = "libro/listar_autor.html"
+    template_name: str = "libro/autor/listar_autor.html"
     queryset = Autor.objects.filter(estado=True)
     context_object_name = "autores"
 
 class ActualizarAutor(UpdateView):
     model = Autor
-    template_name = "libro/crear_autor.html"
+    template_name = "libro/autor/crear_autor.html"
     form_class = forms.AutorForm
     success_url = reverse_lazy("libro:listar_autor")
 
 class CrearAutor(CreateView):
     model = Autor
     form_class = forms.AutorForm
-    template_name = "libro/crear_autor.html"
+    template_name = "libro/autor/crear_autor.html"
     success_url = reverse_lazy("libro:listar_autor")
 
 class EliminarAutor(DeleteView):
     model = Autor
+    template_name = "libro/autor/autor_confirm_delete.html"
 
     def post(self, request, pk, *args, **kwargs):
         object = Autor.objects.get(id=pk)
@@ -36,14 +38,6 @@ class EliminarAutor(DeleteView):
         object.save()
         return redirect("libro:listar_autor")
 
-def eliminarAutor(request, id):
-    try:
-        autor = Autor.objects.get(id=id)
-        if request.method == "POST":
-            autor.estado = False
-            autor.save()
-            return redirect("libro:listar_autor")
-        return render(request, "libro/eliminar_autor.html", {"autor":autor})
-    except ObjectDoesNotExist as e:
-        error = e
-    return render(request, "libro/listar_autor.html", {"error":error})
+class ListarLibro(ListView):
+    model = Libro
+    template_name = "libro/libro/listar_libro.html"
